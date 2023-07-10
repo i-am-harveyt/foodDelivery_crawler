86b1eb957aec9fa75befe5f476bbfd93c57ee5dc
yushin <vxh440@gmail.com>
2023-07-10 22:50:17 +0800

    run path

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # "${1}"
 date
 python3 --version
-cd /home/b08305056/GoogleDrive/Classroom/FoodpandaUbereat/remote_ntu_server/panda_code/
+cd /home/b08305056/fromGit/foodDelivery_crawler
 /bin/python3 getNearShop.py --workerNumShop 10 -debug true
 /bin/python3 getMeau.py --workerNumMenu 1 -debug true
 # python3.8 upload.py
